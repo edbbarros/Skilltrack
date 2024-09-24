bc7a337c8411477fb9a0c204f9e27dc3df500ad7
edbbarros <edbbarros@hotmail.com>
2024-09-24 18:34:14 -0300

    first commit

---
+++ src/main/java/com/skilltrack/employees_api/EmployeesApiApplication.java
@@ -0,0 +1,13 @@
+package com.skilltrack.employees_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/skilltrack/entity/Employees.java
@@ -0,0 +1,76 @@
+package com.skilltrack.entity;
+
+public class Employees {
+    public Employees() {
+
+    }
+
+    int id_employees;
+    String name;
+    String email;
+    String contact;
+    String hardskills;
+    String certifications;
+    String time_of_experience;
+    String user_linkedin;
+
+
+    public int getId_employees() {
+        return id_employees;
+    }
+
+    public void setId_employees(int id_employees) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+    }
+
+    public String getHardskills() {
+        return hardskills;
+    }
+
+    public void setHardskills(String hardskills) {
+        this.hardskills = hardskills;
+
+    }
+
+    public String getCertifications() {
+        return certifications;
+    }
+
+    public void setCertifications(String certifications) {
+    }
+
+    public String getTime_of_experience() {
+        return time_of_experience;
+    }
+
+    public void setTime_of_experience(String time_of_experience) {
+        this.time_of_experience = time_of_experience;
+    }
+
+    public String getUser_linkedin() {
+        return user_linkedin;
+    }
+
+    public void setUser_linkedin(String user_linkedin) {
+    }
+}
